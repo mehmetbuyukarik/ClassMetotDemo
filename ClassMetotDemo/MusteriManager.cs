f6bc652586ccfd616c670b54fd1108da68883760
Mehmet Büyükarık <mbuyukarik@gmail.com>
2021-04-30 16:09:03 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        string AddCustomerSuccessfull = "Müşteri Sisteme Başarıyla Eklendi";
+        string AddCustomerTemplate = "Müşteri Bilgileri";
+
+        public void AddMusteri(Musteri musteri)
+        {
+            Console.WriteLine(AddCustomerTemplate);
+            Console.WriteLine("             ");
+            Console.WriteLine("Id: " + musteri.Id);
+            Console.WriteLine("İsim: " + musteri.FirstName);
+            Console.WriteLine("Soyad: " + musteri.LastName);
+            Console.WriteLine("Hesap No: " + musteri.AccountNumber);
+            Console.WriteLine("             ");
+            Console.WriteLine(AddCustomerSuccessfull);
+        }
+        public void RemoveMusteri(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Sistemden Silindi");
+            Console.WriteLine("Müşteri No: " + musteri.Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            {
+                musteri1.Id = 1;
+                musteri1.FirstName = "Mehmet";
+                musteri1.LastName = "Büyükarık";
+                musteri1.AccountNumber = 26;
+            }
+            Musteri musteri2 = new Musteri();
+            {
+                musteri2.Id = 2;
+                musteri2.FirstName = "Semih";
+                musteri2.LastName = "Büyükarık";
+                musteri2.AccountNumber = 22;
+            }
+            MusteriManager MusteriManager = new MusteriManager();
+
+            MusteriManager.AddMusteri(musteri1);
+            Console.WriteLine("     ");
+            MusteriManager.RemoveMusteri(musteri2);
+
+
+        }
+    }
+}
